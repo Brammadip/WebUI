9cadf16f48b4b5880e2a5ceb060683e2c2c8ecd8
Brammadip <dip.dhaka@gmail.com>
2015-10-19 13:30:35 -0700

    another project adding to git

---
+++ A2/WebContent/js/a2.js
@@ -0,0 +1,17 @@
+function showResults(){
+	var x=5;
+	var y=4;
+	alert (x+y);
+}
+
+function viewResults(){
+	var x=5;
+	var y=4;
+	alert (x*y);
+}
+
+function seeResults(){
+	var x=5;
+	var y=4;
+	alert (x%y);
+}
